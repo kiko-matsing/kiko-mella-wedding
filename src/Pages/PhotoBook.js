b2babb0479f827330e9d46f23f42dd32e67eca9d
Francisco G. Quinola <francisco.quinola@legalmatch.com>
2024-10-10 23:46:34 +0800

    Add Pansamantagal na Photo book

--- src/Pages/PhotoBook.js
+++ src/Pages/PhotoBook.js
@@ -1,87 +1,38 @@
-import React, { useEffect, useState } from "react";
-import { RowsPhotoAlbum } from "react-photo-album";
-import "react-photo-album/rows.css";
-import axios from "axios";
+import { useState } from "react";
+import { Gallery } from "react-grid-gallery";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import { images } from "../Data/ImageList";
+import { motion } from "framer-motion";
+import { fadeDownAnimationVariants } from "../Data/FadeAnimation";
 
-const photos = [
-    { src: "/kikomella5.png", width: 800, height: 600 },
-    { src: "/image2.jpg", width: 1600, height: 900 },
-  ];
-  
+const slides = images.map(({ original, width, height }) => ({
+  src: original,
+  width,
+  height,
+}));
+
 const PhotoBook = () => {
+  const [index, setIndex] = useState(-1);
 
+  const handleClick = (index, item) => setIndex(index);
 
-    useEffect(()=>{
-      window.scrollTo(0, 0);
-    },[])
-
-
-    const [fName, setfName] = useState("");
-    const [lName, setlName] = useState("");
-    const [email, setEmail] = useState("");
-  
-    const handleSubmit = (e) => {
-  
-      e.preventDefault();
-      const serviceId = "service_k1l9heg"
-      const templateId = "template_l4v41nt"
-      const publicKey = "y77VhhP3PrgRq_WAP"
-    
-      const data = {
-        service_id: serviceId,
-        template_id: templateId,
-        user_id: publicKey,
-        template_params: {
-          from_name: 'Kiko and Mella',
-          to_email: email,
-          to_name: `${fName} ${lName}`,
-        }
-      }
-  
-      axios.post("https://api.emailjs.com/api/v1.0/email/send", data)
-          .then((res) => {
-            console.log(res.data);
-            setlName('');
-            setfName('');
-            setEmail('');
-       }
-       )
-       .catch((error) => {
-        console.error('Error Sending Message', error)
-       })
-    }
-  
-    return (
-      <div id="rsvp">
-        <form onSubmit={handleSubmit} className="form">
-          
-          <label className="label">*First Name</label>
-          <input type="text"
-                name="from_name"
-                value={fName}
-                className="input"
-                onChange={(e) => setfName(e.target.value)} />
-          
-          <label className="label">*Last Name</label>
-          <input type="text"
-                name="from_name"
-                value={lName}
-                className="input"
-                onChange={(e) => setlName(e.target.value)} />
-          
-          <label>Email</label>
-          <input type="email"
-                name="email"
-                value={email}
-                className="input"
-                onChange={(e) => setEmail(e.target.value)} />
-          <input type="submit" value="Send" className="submit"/>
-        </form>
-      </div>
-    );
-    
+  return (
+    <div id="photo-book">
+      <h3 className="title">The Couple</h3>
+      <Gallery
+        images={images}
+        onClick={handleClick}
+        enableImageSelection={false}
+      />
+      <Lightbox
+        slides={slides}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+      />
+    </div>
+  );
 }
-
-
 
 export default PhotoBook
